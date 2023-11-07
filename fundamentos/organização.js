67d1398e46a75e7514759ad60189527291ffe6d5
Miguel Estevam <miguelfrancaestevamlopes@gmail.com>
2023-11-07 14:14:30 -0300

    Correções

---
+++ fundamentos/organização.js
@@ -0,0 +1,8 @@
+console.log("Hello world")
+console.log("Olá mundo");
+
+{
+    {
+        console.log("Miguel")
+    }
+}
---
+++ fundamentos/variaveisEconstantes.js
@@ -0,0 +1,3 @@
+var a = 3
+let b = 4 //let é uma forma mais mordena e mais recomendável de usar
+const int = 6
